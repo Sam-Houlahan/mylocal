ce7d540c65ad28502790f22808d44bb265ceaedd
justinvos <justinvosnz@gmail.com>
2017-07-29 10:19:33 +1200

    Addded base notifications code

--- public/sw-test/sw.js
+++ public/sw-test/sw.js
@@ -1,1 +1,8 @@
+console.log("sw")
+this.addEventListener('install', function(event) {
+  console.log("installing")
+});
 
+self.addEventListener('activate', function(event) {
+  console.log('V1 now ready to handle fetches!');
+});
