c09ef7ea393c74c7014e3d8a66c14b53ede53d43
justinvos <justinvosnz@gmail.com>
2017-07-29 11:07:57 +1200

    Removed service worker code for simpilicity

--- client/index.js
+++ client/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom'
 
 import App from './components/App'
 
-const applicationPublicKey = 'BFwsrEdIy5_mZyUYdFQFPnUJe_IkmUSBMjsMnqg1rCZQcpi6So7Pw8UAwnkgokzaHYBvN-spnl0sE9MZjvYhgiA'
-
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <App />,
@@ -12,68 +10,10 @@ document.addEventListener('DOMContentLoaded', () => {
   )
 })
 
-function urlB64ToUint8Array(base64String) {
-  const padding = '='.repeat((4 - base64String.length % 4) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
-    .replace(/_/g, '/');
-
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
+Notification.requestPermission().then(function(permission) {
+  console.log(permission)
+  console.log("Notification permission granted")
+  if (Notification.permission === "granted") {
+    var notification = new Notification("Hi there!");
   }
-  return outputArray;
-}
-
-// Push notifications service worker code
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw-test/sw.js', {scope: '/sw-test/'})
-  .then(function(reg) {
-    // registration worked
-    console.log('Registration succeeded. Scope is ' + reg.scope);
-    console.log(reg)
-
-    console.log(reg.pushManager)
-
-    var options = {
-      userVisibleOnly: true,
-      applicationServerKey: urlB64ToUint8Array(applicationPublicKey)
-    }
-
-    reg.pushManager.subscribe(options)
-    .then(function(subscription) {
-      console.log('User is subscribed.');
-
-      Notification.requestPermission().then(function(permission) {
-        console.log(permission)
-        console.log("Notification permission granted")
-        if (Notification.permission === "granted") {
-          // If it's okay let's create a notification
-          var notification = new Notification("Hi there!");
-        }
-      });
-
-    })
-    console.log(reg.pushManager)
-
-
-    /*​reg.pushManager.subscribe().then(
-      function(pushSubscription) {
-        console.log("got push");
-        console.log(pushSubscription);
-      }, function(error) {
-        // During development it often helps to log errors to the
-        // console. In a production environment it might make sense to
-        // also report information about errors back to the
-        // application server.
-        console.log(error);
-      });
-    */
-
-  }).catch(function(error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
-  });
-}
+});
--- public/sw-test/sw.js
+++
@@ -1,8 +0,0 @@
-console.log("sw")
-this.addEventListener('install', function(event) {
-  console.log("installing")
-});
-
-self.addEventListener('activate', function(event) {
-  console.log('V1 now ready to handle fetches!');
-});
