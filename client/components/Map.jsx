2d17d737beded75cddec8da9843898d8a319d27b
Sam Houlahan <samhoulahan625@gmail.com>
2017-07-30 15:19:04 +1200

    changed location back

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -49,7 +49,7 @@ export default class Map extends React.Component {
   }
 
   moveMapToAuckland (map) {
-    map.setCenter({lat: this.state.userLocation.latitude, lng: this.state.userLocation.longitude})
+    map.setCenter({lat: -36.848461, lng: 174.763336})
     map.setZoom(17)
   }
 
